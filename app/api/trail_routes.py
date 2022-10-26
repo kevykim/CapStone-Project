66db8ddc67c8d1607b5f9796e5ecbe7ba6459ad0
Kevin Kim <kebonkim@gmail.com>
2022-10-25 23:43:42 -0700

    set up trail and review routes api routes

---
+++ app/api/trail_routes.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, jsonify
+from app.models import Trail, db
+from app.forms import TrailForm
+
+trail_routes = Blueprint('trails', __name__)
+
+
+# CREATE
+
+# READ
+
+# UPDATE
+
+# DELETE
