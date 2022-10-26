cec6790423e2d575ed921c15a856689c834e90e2
Kevin Kim <kebonkim@gmail.com>
2022-10-26 14:03:07 -0700

    trail read routes

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+from flask_login import login_required, current_user
 from app.models import Review, db
 from app.forms import ReviewForm
 
--- app/api/trail_routes.py
+++ app/api/trail_routes.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, request
+from flask_login import login_required, current_user
 from app.models import Trail, db
 from app.forms import TrailForm
+from app.api.auth_routes import validation_errors_to_error_messages
 
 trail_routes = Blueprint('trails', __name__)
 
@@ -8,6 +10,19 @@ trail_routes = Blueprint('trails', __name__)
 # CREATE
 
 # READ
+
+#GET ALL TRAILS
+@trail_routes.route('/')
+def get_all_trails():
+    trails = Trail.query.all()
+    return {'trail': [trail.to_dict() for trail in trails]}
+
+@trail_routes.route('/current')
+@login_required
+def get_currentuser_trail():
+    trails = Trail.query.filter(Trail.userId == current_user.id)
+    return {'trail': [trail.to_dict() for trail in trails]}
+    
 
 # UPDATE
 
