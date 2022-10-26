84b60a0b3affaba30fe3f86c7ec8fe5e6684d5bd
Kevin Kim <kebonkim@gmail.com>
2022-10-26 14:32:22 -0700

    get trail by id and added 404 message

--- app/api/trail_routes.py
+++ app/api/trail_routes.py
@@ -11,19 +11,31 @@ trail_routes = Blueprint('trails', __name__)
 
 # READ
 
-#GET ALL TRAILS
+# GET ALL TRAILS
 @trail_routes.route('/')
 def get_all_trails():
     trails = Trail.query.all()
+    if trails is None:
+        return {'errors': 'Trail not found'}, 404
+
     return {'trail': [trail.to_dict() for trail in trails]}
 
+# GET CURRENT USER'S TRAILS
 @trail_routes.route('/current')
 @login_required
 def get_currentuser_trail():
     trails = Trail.query.filter(Trail.userId == current_user.id)
+    if trails is None:
+        return {'errors': 'Trail not found'}, 404
     return {'trail': [trail.to_dict() for trail in trails]}
     
-
+# GET A TRAIL BY ID
+@trail_routes.route('/<int:id>')    
+def get_trail_by_id(id):
+    trails = Trail.query.get(id)
+    if trails is None:
+        return {'errors': 'Trail not found'}, 404
+    return trails.to_dict()
 # UPDATE
 
 # DELETE
