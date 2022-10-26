b8647bae6f03282f856f0d484d0937db550cc87c
Kevin Kim <kebonkim@gmail.com>
2022-10-26 16:49:50 -0700

    create review working

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -1,6 +1,6 @@
-from distutils import log
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_login import login_required, current_user
+from app.api.auth_routes import validation_errors_to_error_messages
 from app.models import Review, db
 from app.forms import ReviewForm
 
@@ -8,6 +8,25 @@ review_routes = Blueprint('reviews', __name__)
 
 
 #CREATE
+
+# CREATE A REVIEW
+@review_routes.route('/trails/<int:id>', methods=["POST"])
+@login_required
+def create_review(id):
+    reviewform = ReviewForm()
+    reviewform['csrf_token'].data = request.cookies['csrf_token']
+    if reviewform.validate_on_submit():
+        created_review = Review(
+            trailId = id,
+            userId = current_user.id,
+            review = reviewform.data['review'],
+            stars = reviewform.data['stars'],
+            reviewImg = reviewform.data['reviewImg']
+        )
+        db.session.add(created_review)
+        db.session.commit()
+        return created_review.to_dict()
+    return {'errors': validation_errors_to_error_messages(reviewform.errors)}, 400
 
 #READ
 
@@ -27,9 +46,6 @@ def get_currentuser_review():
     if reviews is None:
         return {'errors': 'Review not found'}, 404
     return {'review': [review.to_dict() for review in reviews]}
-
-
-
 
 #UPDATE
 
