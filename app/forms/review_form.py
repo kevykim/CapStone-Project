13184c25a8d753bdd03b87b448c969eacbfe2fe0
Kevin Kim <kebonkim@gmail.com>
2022-10-26 12:56:22 -0700

    review form validations

--- app/forms/review_form.py
+++ app/forms/review_form.py
@@ -4,10 +4,18 @@ from wtforms.validators import DataRequired, ValidationError, NumberRange
 from app.models import Review
 
 
+def reviewchecker(form, field):
+    review = field.data
+    if len(review) < 10:
+        raise ValidationError('Reviews must be 10 characters long')
 
+def imagechecker(form, field):
+    image = field.data
+    if 'https' not in image or ('.jpg' not in image) and ('.jpeg' not in image) and ('.gif' not in image) and ('.svg' not in image):
+        raise ValidationError('Please enter a valid URL.')
 
 
 class ReviewForm(FlaskForm):
-    review = TextAreaField('review', validators=[DataRequired()])
-    stars = IntegerField('stars', validators=[DataRequired(), NumberRange(min = 1, max = 5)])
-    reviewImg = StringField('reviewImg')
+    review = TextAreaField('review', validators=[DataRequired(), reviewchecker])
+    stars = IntegerField('stars', validators=[DataRequired(), NumberRange(min = 1, max = 5, message='Please select a star from 1 to 5.')])
+    reviewImg = StringField('reviewImg', validators=[imagechecker])
