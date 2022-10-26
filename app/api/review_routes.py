2afd35fa52b816302a2598afe034158e1b5dfd4b
Kevin Kim <kebonkim@gmail.com>
2022-10-26 16:26:36 -0700

    get current review working

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -1,3 +1,4 @@
+from distutils import log
 from flask import Blueprint
 from flask_login import login_required, current_user
 from app.models import Review, db
@@ -9,6 +10,16 @@ review_routes = Blueprint('reviews', __name__)
 #CREATE
 
 #READ
+
+# GET CURRENT USER'S REVIEWS
+@review_routes.route('/current')
+@login_required
+def get_currentuser_review():
+    reviews = Review.query.filter(Review.userId == current_user.id)
+    if reviews is None:
+        return {'errors': 'Review not found'}, 404
+    return {'review': [review.to_dict() for review in reviews]}
+
 
 #UPDATE
 
