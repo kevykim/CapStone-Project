018367acfbeec1b722192154f0e6c9f42de3009c
Kevin Kim <kebonkim@gmail.com>
2022-10-27 16:10:46 -0700

    homepage styling need to go back

--- react-app/src/components/Trail/HomePage.js
+++ react-app/src/components/Trail/HomePage.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch} from 'react-redux';
+import { NavLink } from 'react-router-dom';
 import { thunkGetTrail } from '../../store/trail';
 
 
@@ -17,50 +18,47 @@ function HomePage() {
 
 
     return (
-        <div>
       <div className="homepage_main">
-        <div className="homepage_image_div">
-          <img
-            style={{ height: "540px", width: "1265px" }}
-            classname="homepage_image"
-            src="https://images.pexels.com/photos/1404713/pexels-photo-1404713.jpeg"
-            alt="home"
-          />
+        <div className="homepage_main">
+          <div className="homepage_image_div">
+            <img
+              style={{ height: "540px", width: "1265px" }}
+              classname="homepage_image"
+              src="https://images.pexels.com/photos/1404713/pexels-photo-1404713.jpeg"
+              alt="home"
+            />
+          </div>
+          <div className="homepage_text">
+            <div>Find your next trail</div>
+          </div>
         </div>
-        <div className="homepage_text">
-          <div>Find your next trail</div>
+        <div className="homepage_empty_div"></div>
+        <div className="local_favorites_text">Local favorites</div>
+        <div className="homepage_arr_div">
+          {trailArr.map((trail, i) => (
+            <div className="homepage_arr_div_inner" key={i}>
+              <div>
+                <NavLink exact to={`/trail/${trail.id}`}>
+                  <img
+                    className="homepage_trail_image"
+                    src={trail.previewImg}
+                    alt="trailimg"
+                  />
+                </NavLink>
+              </div>
+              <div>
+                <div className="homepage_trail_firstline">
+                  <div>{trail.difficulty}&nbsp;</div>
+                  <div>staravg&totalreviews</div>
+                </div>
+                <div className="homepage_trail_name">{trail.name}</div>
+                <div className="homepage_trail_othertext">{trail.resort}</div>
+                <div className="homepage_trail_othertext">{`Length: ${trail.length} mi`}</div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-        <div>Find your local favorites</div>
-        <div>
-            {trailArr.map((trail, i) => (
-                <div key={i}> 
-                <div>
-                    <img className='homepage_trail_image' src={trail.previewImg} alt='trailimg'/>
-                </div>
-                <div>
-                <div>
-                    <div>
-                        {trail.difficulty}
-                    </div>
-                    <div>
-                        star avg and total reviews here
-                    </div>
-                </div>
-                <div>
-                    {trail.name}
-                </div>
-                <div>
-                    {trail.resort}
-                </div>
-                <div>
-                    {`Length: ${trail.length} mi`}
-                </div>
-                </div>
-                </div>
-            ))}
-        </div>
-        </div>
     );
 }
 
