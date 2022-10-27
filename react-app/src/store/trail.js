43f23541bc7123c28749f260bcd3f55f0ddeecd8
Kevin Kim <kebonkim@gmail.com>
2022-10-26 20:24:59 -0700

    trail action types

--- react-app/src/store/trail.js
+++ react-app/src/store/trail.js
@@ -1,6 +1,13 @@
 // TYPES
 
 
+const createTrail = '/trail/createTrail'
+const getTrail = '/trail/getTrail'
+const getCurrentTrail = '/trail/getCurrentTrail'
+const getSingleTrail = '/trail/getSingleTrail'
+const updateTrail = '/trail/updateTrail'
+const deleteTrail = '/trail/deleteTrail'
+
 
 
 
