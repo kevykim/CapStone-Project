7c3a8c32498ec44d8682efa4806de60cab318db2
Kevin Kim <kebonkim@gmail.com>
2022-10-26 20:29:07 -0700

    trail action creators

--- react-app/src/store/trail.js
+++ react-app/src/store/trail.js
@@ -14,7 +14,47 @@ const deleteTrail = '/trail/deleteTrail'
 
 // ACTION CREATORS
 
+const actionCreateTrail = (trail) => {
+    return {
+        type: createTrail,
+        trail
+    }
+}
 
+const actionGetTrail = (trails) => {
+    return {
+        type: getTrail,
+        trails
+    }
+}
+
+const actionGetCurrentTrail = (trails) => {
+    return {
+        type: getCurrentTrail,
+        trails
+    }
+}
+
+const actionGetSingleTrail = (trail) => {
+    return {
+        type: getSingleTrail,
+        trail
+    }
+}
+
+const actionUpdateTrail = (trail) => {
+    return {
+        type: updateTrail,
+        trail
+    }
+}
+
+const actionDeleteTrail = (id) => {
+    return {
+        type: deleteTrail,
+        id
+    }
+}
 
 
 
