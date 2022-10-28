e0fc6a6d4e39631c32394a2f261e4f6e88bf531f
Kevin Kim <kebonkim@gmail.com>
2022-10-27 20:52:07 -0700

    working on trail detail css

--- react-app/src/components/Trail/TrailDetail.js
+++ react-app/src/components/Trail/TrailDetail.js
@@ -1,11 +1,64 @@
+import { useParams } from "react-router-dom"
+import { useSelector, useDispatch } from "react-redux"
+import { useEffect } from "react"
+import { thunkGetSingleTrail } from "../../store/trail"
 
-
-
+import './TrailDetail.css'
 
 function TrailDetail() {
+    const {id} = useParams()
+    const dispatch = useDispatch()
+
+    const user = useSelector(state => state.session.user)
+    const trail = useSelector(state => state.trail[id])
+
+
+    useEffect(() => {
+        dispatch(thunkGetSingleTrail(id))
+    }, [dispatch, id])
+
     return (
-        <h1>Trail Deetail</h1>
-    )
+      <div className="trail_detail_main">
+        <div className="trail_detail_top_text">
+          <div>{trail?.country}</div>
+          <div>{trail?.state}</div>
+          <div>{trail?.resort}</div>
+          <div>{trail?.name}</div>
+        </div>
+        <div className="trail_detail_image_div">
+          <img
+            className="trail_detail_previewImg"
+            src={trail?.previewImg}
+            alt="traildetail"
+          />
+          <div>
+            <div className="trail_detail_bluebar"></div>
+          </div>
+          <div className="trail_detail_circle">
+            <div className="trail_detail_circle_inner">
+              <div className="trail_detail_white_circle"></div>
+              <div className="trail_detail_circle_text">Photos</div>
+            </div>
+            <div className="trail_detail_circle_inner">
+              <div className="trail_detail_white_circle"></div>
+              <div className="trail_detail_circle_text">Share</div>
+            </div>
+            <div className="trail_detail_circle_inner">
+              <div className="trail_detail_white_circle"></div>
+              <div className="trail_detail_circle_text">More</div>
+            </div>
+          </div>
+        </div>
+        <div className="trail_detail_image_text">
+          <div className="trail_detail_image_firstline">{trail?.name}</div>
+          <div className="trail_detail_image_secondline">
+            <div>{trail?.difficulty}</div>
+            <div>review stars and total</div>
+          </div>
+          <div className="trail_detail_image_thirdline">{trail?.resort}</div>
+        </div>
+      </div>
+    );
 }
 
 
