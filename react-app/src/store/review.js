75ab0158bcac694b9ead5103919055b18e9c6109
Kevin Kim <kebonkim@gmail.com>
2022-10-28 11:12:10 -0700

    review action creators

--- react-app/src/store/review.js
+++ react-app/src/store/review.js
@@ -6,21 +6,42 @@ const getCurrentReview = '/review/getCurrentReview'
 const updateTrailReview = '/review/updateTrailReview'
 const deleteTrailReview = '/review/deleteTrailReview'
 
-
-
-
-
-
-
 // ACTION CREATORS
 
+const actionCreateTrailReview = (review) => {
+    return {
+        type: createTrailReview,
+        review
+    }
+}
 
+const actionGetTrailReview = (reviews) => {
+    return {
+        type: getTrailReview,
+        reviews
+    }
+}
 
+const actionGetCurrentReview = (reviews) => {
+    return {
+        type: getCurrentReview,
+        reviews
+    }
+}
 
+const actionUpdateTrailReview = (review) => {
+    return {
+        type: updateTrailReview,
+        review
+    }
+}
 
-
-
-
+const actionDeleteTrailReview = (id) => {
+    return {
+        type: deleteTrailReview,
+        id
+    }
+}
 
 // THUNKS
 
