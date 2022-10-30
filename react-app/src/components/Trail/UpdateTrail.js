f1bb54c0dd2ed03050a3db206ab0eacf6479c7e5
Kevin Kim <kebonkim@gmail.com>
2022-10-30 12:23:42 -0700

    working on update trail css

--- react-app/src/components/Trail/UpdateTrail.js
+++ react-app/src/components/Trail/UpdateTrail.js
@@ -111,163 +111,165 @@ function UpdateTrail() {
   let routeType_choices = ["All-mountain", "Park", "Powder", "Freeride"];
 
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <div>Update trail</div>
-        <div>
-          <div className="update_trail_inputs_div">
-            <input
-              name="name"
-              type="text"
-              placeholder="Name"
-              value={name}
-              onChange={(event) => setName(event.target.value)}
-              className="update_trail_inputs"
-            />
-          </div>
-          <div className="update_trail_inputs_div">
-            <select
-              name="country"
-              value={country}
-              onChange={(event) => setCountry(event.target.value)}
-              className="update_trail_inputs"
-            >
-              <option value="" disabled>
-                Select a country
-              </option>
-              {country_choices.map((country, i) => (
-                <option key={i} value={country}>
-                  {country}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className="update_trail_inputs_div">
-            <select
-              name="state"
-              value={state}
-              onChange={(event) => setState(event.target.value)}
-              className="update_trail_inputs"
-            >
-              <option value="" disabled>
-                Select a state
-              </option>
-              {state_choices.map((state, i) => (
-                <option key={i} value={state}>
-                  {state}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className="update_trail_inputs_div">
-            <select
-              name="resort"
-              value={resort}
-              onChange={(event) => setResort(event.target.value)}
-              className="update_trail_inputs"
-            >
-              <option value="" disabled>
-                Select a resort
-              </option>
-              {resort_choices.map((resort, i) => (
-                <option key={i} value={resort}>
-                  {resort}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className="update_trail_inputs_div">
-            <select
-              name="difficulty"
-              value={difficulty}
-              onChange={(event) => setDifficulty(event.target.value)}
-              className="update_trail_inputs"
-            >
-              <option value="" disabled>
-                Select a difficulty
-              </option>
-              {difficulty_choices.map((difficulty, i) => (
-                <option key={i} value={difficulty}>
-                  {difficulty}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className="update_trail_inputs_div">
-            <textarea
-              name="description"
-              type="text-area"
-              placeholder="Description"
-              value={description}
-              onChange={(event) => setDescription(event.target.value)}
-              className="update_trail_inputs"
-            ></textarea>
-          </div>
-          <div className="update_trail_inputs_div">
-            <input
-              name="length"
-              type="number"
-              placeholder="Length"
-              value={length}
-              onChange={(event) => setLength(event.target.value)}
-              className="update_trail_inputs"
-            />
-          </div>
-          <div className="update_trail_inputs_div">
-            <input
-              name="elevation"
-              type="number"
-              placeholder="Elevation"
-              value={elevation}
-              onChange={(event) => setElevation(event.target.value)}
-              className="update_trail_inputs"
-            />
-          </div>
-          <div className="update_trail_inputs_div">
-            <select
-              name="routeType"
-              value={routeType}
-              onChange={(event) => setRouteType(event.target.value)}
-              className="update_trail_inputs"
-            >
-              <option value="" disabled>
-                Select a Route Type
-              </option>
-              {routeType_choices.map((routeType, i) => (
-                <option key={i} value={routeType}>
-                  {routeType}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className="update_trail_inputs_div">
-            <input
-              name="previewImg"
-              type="text"
-              placeholder="Trail Image"
-              value={previewImg}
-              onChange={(event) => setPreviewImg(event.target.value)}
-              className="update_trail_inputs"
-            />
-          </div>
-          {validations.length > 0 && submitted === true && (
-            <div className="update_trail_error">
-              {validations.map((error, ind) => (
-                <div key={ind}>{error}</div>
-              ))}
+    <div className="update_trail_main">
+      <div className="update_trail_form">
+        <form onSubmit={onSubmit}>
+          <div className="update_trail_header">Update trail</div>
+          <div>
+            <div className="update_trail_inputs_div">
+              <input
+                name="name"
+                type="text"
+                placeholder="Name"
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+                className="update_trail_inputs"
+              />
             </div>
-          )}
-          <div className="update_trail_inputs_div">
-            <button
-              className="update_trail_button_form"
-              type="submit"
-              disabled={validations.length > 0 && submitted}
-            >
-              Update Trail
-            </button>
+            <div className="update_trail_inputs_div">
+              <select
+                name="country"
+                value={country}
+                onChange={(event) => setCountry(event.target.value)}
+                className="update_trail_inputs"
+              >
+                <option value="" disabled>
+                  Select a country
+                </option>
+                {country_choices.map((country, i) => (
+                  <option key={i} value={country}>
+                    {country}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="update_trail_inputs_div">
+              <select
+                name="state"
+                value={state}
+                onChange={(event) => setState(event.target.value)}
+                className="update_trail_inputs"
+              >
+                <option value="" disabled>
+                  Select a state
+                </option>
+                {state_choices.map((state, i) => (
+                  <option key={i} value={state}>
+                    {state}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="update_trail_inputs_div">
+              <select
+                name="resort"
+                value={resort}
+                onChange={(event) => setResort(event.target.value)}
+                className="update_trail_inputs"
+              >
+                <option value="" disabled>
+                  Select a resort
+                </option>
+                {resort_choices.map((resort, i) => (
+                  <option key={i} value={resort}>
+                    {resort}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="update_trail_inputs_div">
+              <select
+                name="difficulty"
+                value={difficulty}
+                onChange={(event) => setDifficulty(event.target.value)}
+                className="update_trail_inputs"
+              >
+                <option value="" disabled>
+                  Select a difficulty
+                </option>
+                {difficulty_choices.map((difficulty, i) => (
+                  <option key={i} value={difficulty}>
+                    {difficulty}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="update_trail_inputs_div">
+              <textarea
+                name="description"
+                type="text-area"
+                placeholder="Description"
+                value={description}
+                onChange={(event) => setDescription(event.target.value)}
+                className="update_trail_textarea"
+              ></textarea>
+            </div>
+            <div className="update_trail_inputs_div">
+              <input
+                name="length"
+                type="number"
+                placeholder="Length"
+                value={length}
+                onChange={(event) => setLength(event.target.value)}
+                className="update_trail_inputs"
+              />
+            </div>
+            <div className="update_trail_inputs_div">
+              <input
+                name="elevation"
+                type="number"
+                placeholder="Elevation"
+                value={elevation}
+                onChange={(event) => setElevation(event.target.value)}
+                className="update_trail_inputs"
+              />
+            </div>
+            <div className="update_trail_inputs_div">
+              <select
+                name="routeType"
+                value={routeType}
+                onChange={(event) => setRouteType(event.target.value)}
+                className="update_trail_inputs"
+              >
+                <option value="" disabled>
+                  Select a Route Type
+                </option>
+                {routeType_choices.map((routeType, i) => (
+                  <option key={i} value={routeType}>
+                    {routeType}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="update_trail_inputs_div">
+              <input
+                name="previewImg"
+                type="text"
+                placeholder="Trail Image"
+                value={previewImg}
+                onChange={(event) => setPreviewImg(event.target.value)}
+                className="update_trail_inputs"
+              />
+            </div>
+            {validations.length > 0 && submitted === true && (
+              <div className="update_trail_error">
+                {validations.map((error, ind) => (
+                  <div key={ind}>{error}</div>
+                ))}
+              </div>
+            )}
+            <div className="update_trail_inputs_div">
+              <button
+                className="update_trail_button_form"
+                type="submit"
+                disabled={validations.length > 0 && submitted}
+              >
+                Update Trail
+              </button>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   );
 }
