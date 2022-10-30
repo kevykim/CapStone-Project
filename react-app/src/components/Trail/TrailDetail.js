dab18f9fb777b1ed0aab28b938c5e081f258ec7b
Kevin Kim <kebonkim@gmail.com>
2022-10-29 18:58:09 -0700

    trail near by me

--- react-app/src/components/Trail/TrailDetail.js
+++ react-app/src/components/Trail/TrailDetail.js
@@ -1,7 +1,7 @@
 import { NavLink, useParams } from "react-router-dom"
 import { useSelector, useDispatch } from "react-redux"
 import { useEffect } from "react"
-import { thunkGetSingleTrail } from "../../store/trail"
+import { thunkGetSingleTrail, thunkGetTrail } from "../../store/trail"
 import CreateReviewModal from "../Review/CreateReviewModal"
 
 import './TrailDetail.css'
@@ -15,12 +15,19 @@ function TrailDetail() {
     const trail = useSelector(state => state.trail[id])
     const review = useSelector(state => state.review)
     const reviewArr = Object.values(review)
-    // console.log(reviewArr)
+
+    //TRAILS NEAR ME SIDEBAR
+    const everyTrail = useSelector(state => state.trail)
+    const everyTrailArr = Object.values(everyTrail)
+
+    const trailsNear = everyTrailArr.filter(trails => trails.resort === trail.resort)
+
 
 
     useEffect(() => {
         dispatch(thunkGetSingleTrail(id))
         dispatch(thunkGetTrailReview(id))
+        dispatch(thunkGetTrail())
     }, [dispatch, id])
 
     return (
@@ -126,39 +133,65 @@ function TrailDetail() {
             </div>
             <div>
               <div className="trail_detail_bottom_inner">
-                <div>
+                <div className="trail_detail_bottom_allreview">
                   <div>review avg</div>
                   <div>review stars</div>
                   <div>{reviewArr.length} Reviews</div>
                 </div>
-                <div>
+                <div className="trail_detail_bottom_writereview">
                   <CreateReviewModal />
                 </div>
               </div>
-              <div className="trail_detail_bottom_inner">
-                <div>
-                  <div>profile pic</div>
-                  <div>
-                  <div>first and last name</div>
-                  <div>review date</div>
-                  </div>
-                </div>
-                <div>
-                  {reviewArr.map((review, i) => (
-                    <div key={i}>
+              <div>
+                {reviewArr.map((review, i) => (
+                  <div className="trail_detail_bottom_inner" key={i}>
+                    <div className="trail_detail_bottom_profileinfo">
+                      <div>profile pic</div>
+                      <div>
+                        <div>first and last name</div>
+                        <div>review date</div>
+                      </div>
+                    </div>
+                    <div>
                       <div>{review.stars}</div>
                       <div>{review.review}</div>
                       <div>review img</div>
                     </div>
-                  ))}
-                </div>
+                  </div>
+                ))}
               </div>
             </div>
           </div>
           <div className="trail_detail_sidebar">
             <div className="trail_detail_near_div">
-              <div>
-                trails near me pics column
+              <div className="trail_detail_sidebar_inner">
+              <div className="trail_detail_near_text">Nearby trails in {trail?.resort}</div>
+                {trailsNear.map((trail, i) => (
+                  <div classname="trail_detail_sidebar_inner_div" key={i}>
+                    <div className="trail_detail_sidebar_image_div">
+                      <NavLink exact to={`/trails/${trail.id}`}>
+                        <img
+                          className="trail_detail_sidebar_image"
+                          src={trail.previewImg}
+                          alt="nearme"
+                        />
+                      </NavLink>
+                      <div className="trail_detail_sidebar_image_info">
+                      <div className="trail_detail_sidebar_firstline">
+                        <div>{trail.difficulty}&nbsp;</div>
+                        <div>staravg&totalreviews</div>
+                      </div>
+                      <div className="trail_detail_sidebar_name">{trail.name}</div>
+                      <div className="trail_detail_sidebar_othertext">
+                        {trail.resort}
+                      </div>
+                      <div className="trail_detail_sidebar_othertext">{`Length: ${trail.length} mi`}</div>
+                      </div>
+                    </div>
+
+
+                  </div>
+                ))}
               </div>
             </div>
           </div>
