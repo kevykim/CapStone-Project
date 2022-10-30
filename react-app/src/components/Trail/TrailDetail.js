d882ee008745bda0c0857f84263ce10a95138cb5
Kevin Kim <kebonkim@gmail.com>
2022-10-29 17:58:35 -0700

    trail details css

--- react-app/src/components/Trail/TrailDetail.js
+++ react-app/src/components/Trail/TrailDetail.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom"
+import { NavLink, useParams } from "react-router-dom"
 import { useSelector, useDispatch } from "react-redux"
 import { useEffect } from "react"
 import { thunkGetSingleTrail } from "../../store/trail"
@@ -15,7 +15,7 @@ function TrailDetail() {
     const trail = useSelector(state => state.trail[id])
     const review = useSelector(state => state.review)
     const reviewArr = Object.values(review)
-    console.log(reviewArr)
+    // console.log(reviewArr)
 
 
     useEffect(() => {
@@ -27,9 +27,39 @@ function TrailDetail() {
       <>
         <div className="trail_detail_main">
           <div className="trail_detail_top_text">
-            <div>{trail?.country}</div>
-            <div>{trail?.state}</div>
-            <div>{trail?.resort}</div>
+            <div>
+              <NavLink
+                style={{ textDecoration: "none", color: "rgb(132, 132, 132)" }}
+                classname="trail_detail_navlink"
+                exact
+                to={`/${trail?.country}`}
+              >
+                {trail?.country}
+              </NavLink>
+            </div>
+            <div>&nbsp;›&nbsp;</div>
+            <div classname="trail_detail_navlink">
+              <NavLink
+                style={{ textDecoration: "none", color: "rgb(132, 132, 132)" }}
+                exact
+                to={`/${trail?.state}`}
+                classname="trail_detail_navlink"
+              >
+                {trail?.state}
+              </NavLink>
+            </div>
+            <div>&nbsp;›&nbsp;</div>
+            <div>
+              <NavLink
+                style={{ textDecoration: "none", color: "rgb(132, 132, 132)" }}
+                classname="trail_detail_navlink"
+                exact
+                to={`/${trail?.resort}`}
+              >
+                {trail?.resort}
+              </NavLink>
+            </div>
+            <div>&nbsp;›&nbsp;</div>
             <div>{trail?.name}</div>
           </div>
           <div className="trail_detail_image_div">
@@ -65,47 +95,74 @@ function TrailDetail() {
             <div className="trail_detail_image_thirdline">{trail?.resort}</div>
           </div>
         </div>
-          <div>
-            <div>{trail?.description}</div>
+        <div className="trail_detail_bottom_box">
+          <div className="trail_detail_bottom_second">
+            <div className="trail_detail_bottom_inner">
+              <div className="trail_detail_bottom_description">
+                {trail?.description}
+              </div>
+            </div>
+            <div className="trail_detail_bottom_inner">
+              <div className="trail_detail_bottom_info_divs">
+                <div className="trail_detail_bottom_firstline">Length</div>
+                <div className="trail_detail_bottom_secondline">
+                  {trail?.length} mi
+                </div>
+              </div>
+              <div className="trail_detail_bottom_info_divs">
+                <div className="trail_detail_bottom_firstline">
+                  Elevation gain
+                </div>
+                <div className="trail_detail_bottom_secondline">
+                  {trail?.elevation} ft
+                </div>
+              </div>
+              <div className="trail_detail_bottom_info_divs">
+                <div className="trail_detail_bottom_firstline">Route Type</div>
+                <div className="trail_detail_bottom_secondline">
+                  {trail?.routeType}
+                </div>
+              </div>
+            </div>
             <div>
-              <div>
-                <div>Length</div>
-                <div>{trail?.length} mi</div>
-              </div>
-              <div>
-                <div>Elevation gain</div>
-                <div>{trail?.elevation} ft</div>
-              </div>
-              <div>
-                <div>Route Type</div>
-                <div>{trail?.routeType}</div>
-              </div>
-              <div>
+              <div className="trail_detail_bottom_inner">
                 <div>
-                  review avg and review total
+                  <div>review avg</div>
+                  <div>review stars</div>
+                  <div>{reviewArr.length} Reviews</div>
                 </div>
                 <div>
-                    <CreateReviewModal />
+                  <CreateReviewModal />
                 </div>
-                <div>firstname and lastname profile here</div>
+              </div>
+              <div className="trail_detail_bottom_inner">
+                <div>
+                  <div>profile pic</div>
+                  <div>
+                  <div>first and last name</div>
+                  <div>review date</div>
+                  </div>
+                </div>
                 <div>
                   {reviewArr.map((review, i) => (
                     <div key={i}>
-                      <div>
-                        {review.stars}
-                        </div>
-                        <div>
-                          {review.review}
-                        </div>
+                      <div>{review.stars}</div>
+                      <div>{review.review}</div>
+                      <div>review img</div>
                     </div>
                   ))}
                 </div>
               </div>
             </div>
-            <div>
-                trails near me on right side
+          </div>
+          <div className="trail_detail_sidebar">
+            <div className="trail_detail_near_div">
+              <div>
+                trails near me pics column
+              </div>
             </div>
           </div>
+        </div>
       </>
     );
 }
