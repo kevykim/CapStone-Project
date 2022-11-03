f020a45d95febd04dd526432690b702268d0ed53
Kevin Kim <kebonkim@gmail.com>
2022-11-03 16:53:30 -0700

    added footers

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -2,6 +2,11 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { NavLink, Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
+import HomePageloginModal from '../Trail/HomePageloginModal';
+import HomePageloginM from '../Trail/HomePageloginM';
+
+import logo from "./WinterTrail_logo.png";
+
 
 import './LoginForm.css'
 
@@ -47,6 +52,141 @@ const LoginForm = () => {
               "https://images.pexels.com/photos/7165180/pexels-photo-7165180.jpeg";
           }}
         />
+        <div className="footer_main">
+          <div className="footer_main">
+            <div className="footer_logo_div">
+              <img
+                src={logo}
+                alt="logo"
+                onError={(e) => {
+                  e.currentTarget.src = { logo };
+                }}
+              />
+              <div className="footer_logo_text">&nbsp;WinterTrails</div>
+            </div>
+            <div className="footer_first_row">
+              <div>
+                <div className="footer_first_row_header">Explore</div>
+                {/* <div>Countries</div>
+                <div>States</div> */}
+                <div className="footer_first_innertext">
+                  <NavLink className="footer_first_navlink" exact to="/resorts">
+                    Resorts
+                  </NavLink>
+                </div>
+                <div className="footer_first_innertext">
+                  <NavLink
+                    className="footer_first_navlink"
+                    exact
+                    to="/alltrails"
+                  >
+                    Trails
+                  </NavLink>
+                </div>
+              </div>
+              <div>
+                <div className="footer_first_row_header">Trails</div>
+                <div className="footer_first_innertext">
+                  {!user ? (
+                    <div>
+                      <HomePageloginModal />
+                    </div>
+                  ) : (
+                    <div>
+                      <NavLink
+                        className="footer_first_navlink"
+                        exact
+                        to="/trails"
+                      >
+                        My trails
+                      </NavLink>
+                    </div>
+                  )}
+                </div>
+                <div className="footer_first_innertext">
+                  {!user ? (
+                    <div>
+                      <HomePageloginM />
+                    </div>
+                  ) : (
+                    <div>
+                      <NavLink
+                        className="footer_first_navlink"
+                        exact
+                        to="/trails/new"
+                      >
+                        Create trail
+                      </NavLink>
+                    </div>
+                  )}
+                </div>
+              </div>
+              <div>
+                <div>
+                  <div className="footer_first_row_header">Connections</div>
+                  <div>
+                    <div>
+                      <i
+                        style={{ color: "rgb(186,194,208)" }}
+                        className="fa-brands fa-linkedin"
+                      >
+                        &nbsp;
+                      </i>
+                      <a
+                        className="footer_first_navlink"
+                        href="https://www.linkedin.com/in/kevin-kim-a88429150"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Linkedin
+                      </a>
+                    </div>
+                    <div>
+                      <i
+                        style={{ color: "rgb(186,194,208)" }}
+                        className="fa-brands fa-square-github"
+                      >
+                        &nbsp;
+                      </i>
+                      <a
+                        className="footer_first_navlink"
+                        href="https://github.com/kevykim"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Github
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="footer_last_main">
+              <div className="footer_last_left">
+                <div>2022 WinterTrails&nbsp;•&nbsp;</div>
+                <div>Inspired by&nbsp;</div>
+                <a
+                  className="footer_last_navlink"
+                  href="https://www.alltrails.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  AllTrails
+                </a>
+                &nbsp;•&nbsp;
+                <a
+                  className="footer_last_navlink"
+                  href="https://github.com/kevykim/CapStone-Project"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  More info
+                </a>
+              </div>
+              <div>{`English (US)`}</div>
+            </div>
+          </div>
+        </div>
       </div>
       <form className="log_in_form" onSubmit={onLogin}>
         <div className="let_going_text">Log in and let's get going</div>
