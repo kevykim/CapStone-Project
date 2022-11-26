6dc0a7af0fac1fc7cb1c07d13f76d4c9651f1748
Kevin Kim <kebonkim@gmail.com>
2022-11-25 17:14:36 -0800

    working on searchbar

---
+++ react-app/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,139 @@
+import { useState, useEffect} from "react";
+import { NavLink } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+import { thunkGetTrail } from "../../store/trail";
+
+
+import './SearchBar.css'
+
+
+
+function SearchBar() {
+    const dispatch = useDispatch()
+    const trails = useSelector((state) => state.trail)
+    const trailsArr = Object.values(trails)
+
+    const [searchWord, setSearchWord] = useState("");
+    const [showDropdown, setShowDropdown] = useState(false);
+    const [searchResult, setSearchResult] = useState([]);
+
+    const results = (word) => {
+       let arr = []
+       trailsArr.forEach((trail, i) => {
+        if (
+          trail["name"].toLowerCase().includes(word.toLowerCase()) ||
+          trail["resort"].toLowerCase().includes(word.toLowerCase())
+        ) {
+          arr.push(trail);
+        }
+        })
+        return arr
+
+
+    /*
+    const array = spotsArr.map((spot, i) => {
+        if (spot[i].city.toLowerCase().includes(word.toLowerCase())) {
+            return;
+        }
+    })
+    */
+    };
+
+    useEffect(() => {
+        dispatch(thunkGetTrail())
+    },[dispatch])
+
+    useEffect(() => {
+      if (searchWord?.length) {
+        setShowDropdown(true);
+        setSearchResult(results(searchWord));
+      } else {
+        setShowDropdown(false);
+        setSearchResult([]);
+      }
+    }, [searchWord]);
+
+
+
+    return (
+      <>
+        <div className="searchbar_main">
+          <input
+            type="text"
+            className="searchbar_input"
+            placeholder="Search by city, resort, or trail name"
+            onChange={(e) => setSearchWord(e.target.value)}
+            value={searchWord}
+          />
+          <div type="submit" className="searchbar_magnify">
+            <i className="fa-solid fa-magnifying-glass fa-md "></i>
+          </div>
+          <div className="searchbar_arrowright">
+            <i className="fa-solid fa-arrow-right"></i>
+          </div>
+        </div>
+
+        {showDropdown && searchResult?.length > 0 && (
+          <div className="searchbar_dropmenu">
+            {searchResult.map((trail) => (
+              <NavLink
+                to={`/spots/${trail.id}`}
+                className="searchbar_dropmenu_navlink"
+                onClick={() => setSearchWord("")}
+              >
+                <div className="searchbar_dropmenu_inner">
+                  {trail["resort"] === "Boreal Mountain Resort" && (
+                    <div className="searchbar_dropmenu_text">
+                      <div className="searchbar_dropdown_icons">
+                        <i className="fa-solid fa-mountain fa-xl"></i>
+                      </div>
+                      <div>
+                        <div>{`${trail["name"]}, ${trail["resort"]}`}</div>
+                        <div>{`${trail["state"]}, ${trail["country"]}`}</div>
+                      </div>
+                    </div>
+                  )}
+                  {trail["resort"] === "Heavenly Lake Tahoe" && (
+                    <div className="searchbar_dropmenu_text">
+                      <div>{`${trail["name"]}, ${trail["resort"]}`}</div>
+                      {`${trail["state"]}, ${trail["country"]}`}
+                    </div>
+                  )}
+                  {trail["resort"] === "Mammoth Mountain" && (
+                    <div className="searchbar_dropmenu_text">
+                      <div>{`${trail["name"]}, ${trail["resort"]}`}</div>
+                      {`${trail["state"]}, ${trail["country"]}`}
+                    </div>
+                  )}
+                  {trail["resort"] === "Bear Mountain" && (
+                    <div className="searchbar_dropmenu_text">
+                      <div>{`${trail["name"]}, ${trail["resort"]}`}</div>
+                      {`${trail["state"]}, ${trail["country"]}`}
+                    </div>
+                  )}
+                  {trail["resort"] === "Mountain High" && (
+                    <div className="searchbar_dropmenu_text">
+                      <div>{`${trail["name"]}, ${trail["resort"]}`}</div>
+                      {`${trail["state"]}, ${trail["country"]}`}
+                    </div>
+                  )}
+                </div>
+              </NavLink>
+            ))}
+          </div>
+        )}
+
+        {showDropdown && !searchResult?.length && (
+          <div className="searchbar_dropmenu">
+            <div className="searchbar_nosearchfound">
+              No results found for :{" "}
+              <span className="searchbar_nosearchspan">{searchWord}</span>
+            </div>
+          </div>
+        )}
+      </>
+    );
+}
+
+export default SearchBar;
